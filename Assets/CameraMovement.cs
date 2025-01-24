cb1c3bd3ff743684f98182ec985e9b704ec3a461
GVanBT <36698@ma-web.nl>
2025-01-24 10:13:56 +0100

    init commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private const float YMin = -50.0f;
+    private const float YMax = 50.0f;
+
+    public Transform lookAt;
+
+    public Transform Player;
+
+    public float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity = 4.0f;
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.forward * Time.deltaTime * 10;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.right * Time.deltaTime * 10;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += -transform.forward * Time.deltaTime * 10;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.right * Time.deltaTime * 10;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += transform.up * Time.deltaTime * 50;
+        }
+
+        float mouseXMovement = Input.GetAxis("Mouse X");
+        Debug.Log(mouseXMovement);
+        transform.Rotate(0, mouseXMovement * 10, 0);
+    }
+}
